9fbf433b9153884efbd60201b700616e05794c34
Ujjawal <ujjawal.khare@dream11.com>
2023-12-03 17:19:19 +0530

    --wip-- [skip ci]

--- src/producer/producer_base_class.py
+++ src/producer/producer_base_class.py
@@ -1,10 +1,9 @@
 import json
+from typing import List
 
 from kafka import KafkaProducer
 import ray
-from ray.data.block import Block
 import pyarrow
-from ray.types import ObjectRef
 
 
 class ProducerBaseClass:
@@ -12,9 +11,8 @@ class ProducerBaseClass:
         self.topic = topic
         self.producer = KafkaProducer(bootstrap_servers=bootstrap_servers, **kwargs)
 
-    def send_messages(self, message_refs: ObjectRef["pyarrow.Table"]):
-        messages = ray.data.from_arrow_refs([message_refs])
-        for message in messages.iter_rows():
+    def send_messages(self, messages: List[dict]):
+        for message in messages:
             self.producer.send(self.topic, json.dumps(message).encode("utf-8"))
 
     def close(self):
--- src/producer_manager.py
+++ src/producer_manager.py
@@ -1,29 +1,51 @@
+import ray
 from ray.data import Dataset
+
 
 from src.producer.producer import Producer
 from src.producer.producer_actor import ProducerActorClass
 
 
 class KafkaProducerManager:
-    def __init__(self, bootstrap_servers: str, topic: str, **kwargs):
+    def __init__(self, bootstrap_servers: str, topic: str, batch_size: int, **kwargs):
         self.bootstrap_servers = bootstrap_servers
         self.topic = topic
         self._producer_actor = ProducerActorClass.remote(
             bootstrap_servers=self.bootstrap_servers, topic=self.topic, **kwargs
         )
+        self.batch_size = batch_size
         self._producer = Producer(
             bootstrap_servers=self.bootstrap_servers, topic=self.topic, **kwargs
         )
 
     def send_messages(self, df: Dataset, is_actor=True):
-        blocks = df.to_arrow_refs()
-        responses = []
-        for block in blocks:
+        try:
+            responses = []
+            rows = []
+
+            def flush_to_kafka(messages):
+                if is_actor:
+                    resp = self._producer_actor.send_messages.remote(messages)
+                    responses.append(resp)
+                else:
+                    resp = self._producer.send_messages(messages)
+                    responses.append(resp)
+
+            for row in df.iter_rows():
+                rows.append(row)
+
+                if len(rows) == self.batch_size:
+                    flush_to_kafka(rows)
+                    rows = []
+            flush_to_kafka(rows)
+
             if is_actor:
-                resp = self._producer_actor.send_messages.remote(block)
-            else:
-                resp = self._producer.send_messages(block)
-            responses.append(resp)
+                print("Waiting for responses", len(responses), responses)
+                ray.get(responses)
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            print("Exception occurred in send_messages in producer manager", e)
 
     def close(self):
         self._producer_actor.close.remote()

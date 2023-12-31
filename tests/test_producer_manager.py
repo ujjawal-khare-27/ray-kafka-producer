d44f21c5921ff779f45ebad3932eb0b3a48bd293
ujjawal-khare-27 <138279087+ujjawal-khare-27@users.noreply.github.com>
2023-12-31 19:44:22 +0530

    readme updated (#4)
    Co-authored-by: Ujjawal <ujjawal.khare@dream11.com>

--- tests/test_producer_manager.py
+++ tests/test_producer_manager.py
@@ -12,10 +12,13 @@ class TestProducerManager(unittest.TestCase):
         self.KafkaProducerManager = KafkaProducerManager(
             bootstrap_servers="localhost:9092",
             topic="test",
-            actor_pool_size=4,
+            actor_pool_size=12,
             num_cpu=0.25,
         )
+        self.create_ray_df()
+        self.create_pandas_df()
 
+    def create_ray_df(self):
         data = {}
         for i in range(100):
             temp = []
@@ -23,22 +26,49 @@ class TestProducerManager(unittest.TestCase):
                 temp.append(j)
             data[str(i)] = temp
         ray.init(ignore_reinit_error=True)
-        print("Available resources", ray.available_resources())
         pd_df = pd.DataFrame(data)
         self.ray_df = ray.data.from_pandas(pd_df)
-        print("len data", self.ray_df.count())
+        print("Ray df length", self.ray_df.count())
 
-    def test_producer_manager_serial_producer(self):
+    def create_pandas_df(self):
+        data = {}
+        for i in range(100):
+            temp = []
+            for j in range(40000):
+                temp.append(j)
+            data[str(i)] = temp
+        self.pd_df = pd.DataFrame(data)
+        print("Pandas df length", len(self.pd_df))
+
+    def test_producer_manager_serial_producer_ray_df(self):
+        """
+        This test is to check the time taken to send messages using serial producer for ray df
+        :return:
+        """
         t1 = time.time()
         self.KafkaProducerManager.flush_ray_df(self.ray_df, is_actor=False)
         t2 = time.time()
-        print(f"Time taken in test_producer_manager_serial_producer: {t2 - t1}")
+        print(f"Time taken in test_producer_manager_serial_producer for ray df: {t2 - t1}")
 
-    def test_producer_manager_actor_producer(self):
+    def test_producer_manager_actor_producer_ray_df(self):
+        """
+        This test is to check the time taken to send messages using actor producer for ray df
+        :return:
+        """
         t1 = time.time()
         self.KafkaProducerManager.flush_ray_df(self.ray_df, is_actor=True)
         t2 = time.time()
-        print(f"Time taken in test_producer_manager_actor_producer: {t2 - t1}")
+        print(f"Time taken in test_producer_manager_actor_producer for ray df: {t2 - t1}")
+
+    def test_producer_manager_actor_producer_pandas_df(self):
+        """
+        This test is to check the time taken to send messages using actor producer for pandas df
+        :return:
+        """
+        t1 = time.time()
+        self.KafkaProducerManager.flush_pandas_df(self.pd_df)
+        t2 = time.time()
+        print(f"Time taken in test_producer_manager_actor_producer for pandas df: {t2 - t1}")
 
     def tearDown(self):
         self.KafkaProducerManager.close()
